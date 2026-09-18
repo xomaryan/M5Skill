9b3e6c5fb069f695fdf74426f87373f340cbb04a
AnnaMaria Borosovska <40987@ma-web.nl>
2026-09-18 10:23:54 +0200

    rootElement voor mijn artikel

---
+++ SocialMedia/main.js
@@ -0,0 +1,23 @@
+let articleList = document.querySelector("articleListId");
+
+let rootElement = document.createElement("article");
+rootElement.classList.add("post");
+
+rootElement.innerHTML = `
+    <figure class="postImage">
+                <img src="https://www.cats.org.uk/media/yhxbyvoy/230215case104.jpg" alt="img">
+           </figure>
+
+           <footer class="postFooter">
+                <div class="buttons">
+                    <button class="Likes">Likes</button>
+                    <button class="Comments">Comments</button>
+
+                    <p class="postText">
+                        <strong>Naam:</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus deserunt, asperiores quas voluptatibus impedit, sapiente quisquam iste quaerat iusto itaque fugiat consectetur, molestiae repudiandae fuga. Maxime delectus soluta quasi voluptas.
+                    </p>
+                </div>
+           </footer>
+`;
+
+articleList.appendChild(rootElement);
