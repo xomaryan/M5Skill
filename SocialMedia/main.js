661391a679723d7754bda22dc300302984ad7fed
AnnaMaria Borosovska <40987@ma-web.nl>
2026-09-18 15:23:01 +0200

    inleveren

--- SocialMedia/main.js
+++ SocialMedia/main.js
@@ -6,22 +6,6 @@ let rootElement = document.createElement("article");
 rootElement.classList.add("post");
 
     rootElement.innerHTML = `
-        <header class="postHeader">
-            <h2>${title}</h2>
-        </header>
-            <figure class="postImage">
-                <img src="https://www.cats.org.uk/media/yhxbyvoy/230215case104.jpg" alt="img"></figure>
-
-            <footer class="postFooter">
-                <div class="buttons">
-                    <button class="Likes">Likes</button>
-                    <button class="Comments">Comments</button>
-
-                    <p class="postText">
-                        <strong>Naam:</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus deserunt, asperiores quas voluptatibus impedit, sapiente quisquam iste quaerat iusto itaque fugiat consectetur, molestiae repudiandae fuga. Maxime delectus soluta quasi voluptas.
-                    </p>
-                </div>
-            </footer>
     `;
     articleList.appendChild(rootElement);
 }
