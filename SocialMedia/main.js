7563229d08c4c384965785bd208a338f11a2b614
AnnaMaria Borosovska <40987@ma-web.nl>
2026-09-25 09:53:23 +0200

    main.js ingepast

--- SocialMedia/main.js
+++ SocialMedia/main.js
@@ -1,11 +1,45 @@
 let articleList = document.querySelector("#articleListId");
 
-function createPost(title, imgUrl, text) {
 
 
+function createPost(DataTransferItemList, imgUrl) {
+    let rootElement = document.createElement("article");
+    rootElement.classList.add("post");
+    
+    rootElement.innerHTML = `
+        <header class="postHeader">
+                    <span class="username">${title}</span>
+                </header>
+
+                <figure class="postImage">
+                    <img src="https://www.cats.org.uk/media/yhxbyvoy/230215case104.jpg" alt="kat">
+                </figure>
+
+                <footer class="postFooter">
+                    <div class="buttons">
+                        <div class="left-buttons">
+                            <button class="btn-icon" aria-label="Likes">❤️</button>
+                            <button class="btn-icon" aria-label="Reageren">💬</button>
+                            <button class="btn-icon" aria-label="Delen">✈️</button>
+                        </div>
+                        <button class="btn-icon" aria-label="Opslaan">🔖</button>
+                    </div>
+
+                    <p class="likesText"><strong>142 Likes</strong></p>
+
+                    <p class="postText">
+                        <strong>naam</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi non qui repudiandae obcaecati unde dolor quo nobis saepe alias nisi blanditiis cumque maxime, rem sit a quibusdam ea! Tempore, laudantium?
+                    </p>
+
+                    <div class="commentsSection">
+                        <span class="timeAgo">2 UUR GELEDEN</span>
+                    </div>
+                </footer>
+        `;
+
+        acticleList.appendChild(rootElement);
+}
 
 createPost(
-    "Naam",
-    "https://www.cats.org.uk/media/yhxbyvoy/230215case104.jpg",
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus deserunt, asperiores quas voluptatibus impedit, sapiente quisquam iste quaerat iusto itaque fugiat consectetur, molestiae repudiandae fuga. Maxime delectus soluta quasi voluptas."
+    "naam"
 );
